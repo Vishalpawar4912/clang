75f684452e4696c78e030635f5cc9900daf418f6
Vishalpawar4912 <pawarvvishal201@gmail.com>
2024-07-01 13:46:51 +0530

    Add books switch-case problem and solution

--- 20240628/Book/book7.c
+++ 20240628/Book/book7.c
@@ -1,8 +1,8 @@
 // Write a program having a menu with the following options and corresponding actions :
-//1.Area of Square : Accept length , Compute area of square and print. area : l * l;
-//2.Area of Rectangle : Accept length and breadth , Compute area of rectangle and print. area : l * b;
-//3.Area of Triangle : Accept base and height , Compute area of tringle and print. area : (b * h) / 2;
-#include<stdio.h>
+// 1.Area of Square : Accept length , Compute area of square and print. area : l * l;
+// 2.Area of Rectangle : Accept length and breadth , Compute area of rectangle and print. area : l * b;
+// 3.Area of Triangle : Accept base and height , Compute area of tringle and print. area : (b * h) / 2;
+#include <stdio.h>
 int main()
 {
     float area, length, breadth, base, height;
@@ -15,31 +15,30 @@ int main()
         printf("\n3. Area of Triangle");
         printf("\nEnter your choice :");
         scanf("%d", &ch);
-        switch(ch)
+        switch (ch)
         {
-            case 1://Area of Square
-                printf("\nEnter the Length: ");
-                scanf("%f", &length);
-                area = length * length;
-                printf("\n Area of Square: %.2f", area);
-                break;
-            case 2://Area of Rectangle
-                printf("\nEnter the Length: ");
-                scanf("%f", &length);
-                printf("\nEnter the Breadth: ");
-                scanf("%f", &breadth);
-                area = length * breadth;
-                printf("\n Area of Rectangle: %.2f", area);
-                break;
-            case 3://Area of Triangle
-                printf("\nEnter the Base: ");
-                scanf("%f", &base);
-                printf("\nEnter the Height: ");
-                scanf("%f", &height);
-                area = (base * height) / 2;
-                printf("\n Area of Triangle: %.2f", area);
-                break;
+        case 1: // Area of Square
+            printf("\nEnter the Length: ");
+            scanf("%f", &length);
+            area = length * length;
+            printf("\n Area of Square: %.2f", area);
+            break;
+        case 2: // Area of Rectangle
+            printf("\nEnter the Length: ");
+            scanf("%f", &length);
+            printf("\nEnter the Breadth: ");
+            scanf("%f", &breadth);
+            area = length * breadth;
+            printf("\n Area of Rectangle: %.2f", area);
+            break;
+        case 3: // Area of Triangle
+            printf("\nEnter the Base: ");
+            scanf("%f", &base);
+            printf("\nEnter the Height: ");
+            scanf("%f", &height);
+            area = (base * height) / 2;
+            printf("\n Area of Triangle: %.2f", area);
+            break;
         }
     } while (ch != 4);
-    
 }
--- 20240628/Book/book9.c
+++ 20240628/Book/book9.c
@@ -1,7 +1,7 @@
 // write a program having menu that has three options: add, subtract or multiply two fractions.
 // The two fractions and the options are taken as input and the result is displayed as output.
 // Each fraction is read as two integers, numerator and denominator.
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
     int n1, n2, m1, m2;
@@ -16,19 +16,22 @@ int main()
     scanf("%d", &m2);
     printf("1.Addition\n2.Subtract\n3.Multiplication :");
     scanf("%d", &ch);
-    switch(ch)
+    switch (ch)
     {
-        case 1: num = (n1 * m2) + (n2 * m1);
-                den = (n2 * m2);
-                printf("Addition is %d / %d", num, den);
-                break;
-        case 2: num = (n1 * m2) - (n2 * m1);
-                den = (n2 * m2);
-                printf("Sbtraction is %d / %d", num, den);
-                break;
-        case 3: num = (n1 * m1);
-                den = (n2 * m2);
-                printf("Multiplication is %d / %d", num, den);
-                break;
+    case 1:
+        num = (n1 * m2) + (n2 * m1);
+        den = (n2 * m2);
+        printf("Addition is %d / %d", num, den);
+        break;
+    case 2:
+        num = (n1 * m2) - (n2 * m1);
+        den = (n2 * m2);
+        printf("Sbtraction is %d / %d", num, den);
+        break;
+    case 3:
+        num = (n1 * m1);
+        den = (n2 * m2);
+        printf("Multiplication is %d / %d", num, den);
+        break;
     }
 }

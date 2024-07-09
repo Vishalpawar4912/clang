0c3f1c7f6943f3b2ed71b31e8fd0baf68a82ed54
Vishalpawar4912 <pawarvvishal201@gmail.com>
2024-07-09 22:40:10 +0530

    Add while-loop problem and solution

--- 20240708/while-loop/Set B/while2.c
+++ 20240708/while-loop/Set B/while2.c
@@ -1,1 +1,17 @@
 // 2. Write a program to accept real number x and integer n and calculate the sum of first n terms of the series x+ 3x+5x+7x+… 
+#include<stdio.h>
+int main()
+{
+    float x, sum = 0;
+    int n, i = 1;
+    printf("Enter the value of Real Number X:");
+    scanf("%f", &x);
+    printf("Enter the value of n:");
+    scanf("%d", &n);
+    while (i <= n)
+    {
+        sum = sum + (2 * i - 1) * x;
+        i++;
+    }
+    printf("Sum of Series is : %.f", sum);
+}
--- 20240708/while-loop/Set B/while4.c
+++ 20240708/while-loop/Set B/while4.c
@@ -1,1 +1,29 @@
 // 4. Write a program to accept characters till the user enters EOF and count number of alphabets and digits entered.
+#include<stdio.h>
+int main()
+{
+    char str[100];
+    int alp, digit, splch, i;
+    alp = digit = splch = i = 0;
+    printf("Input the String :");
+    gets(str);
+    while (str[i] != '\0')
+    {
+        if(str[i] >= 'a' && str[i] <= 'z' || str[i] >= 'A' && str[i] <= 'Z')
+        {
+            alp++;
+        }
+        else if(str[i] >= '0' && str[i] <= '9')
+        {
+            digit++;
+        }
+        else if(str[i] != ' ' && str[i] != '\n')
+        {
+            splch++;
+        }
+        i++;
+    }
+    printf("Number of Alphabets in the String is :%d\n", alp);
+    printf("Number of Digits in the String is :%d\n", digit);
+    printf("Number of Special Characters in the String is :%d\n", splch);
+}

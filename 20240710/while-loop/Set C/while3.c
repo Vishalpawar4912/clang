64be41cf727959a6fa7e8faca04b7bd0d41073de
Vishalpawar4912 <pawarvvishal201@gmail.com>
2024-07-10 12:48:48 +0530

    Add while-loop problem and solution

---
+++ 20240710/while-loop/Set C/while3.c
@@ -0,0 +1,3 @@
+//3. A train station A at 4.00 a.m. and travels at 80kmph. After every 30 minutes, it reaches a station where it halts for 10 minutes. 
+//it reaches its final destination B at 1.00 p.m.. Display a table showing its arrival and departure time at every intermediate station. 
+//also calculate the total distance between A and B.

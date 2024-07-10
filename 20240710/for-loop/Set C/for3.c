268e59f34def8e79d53aff3e6a867f3dafb15065
Vishalpawar4912 <pawarvvishal201@gmail.com>
2024-07-10 14:34:48 +0530

    Add for-loop problem and solution

--- 20240710/for-loop/Set C/for3.c
+++ 20240710/for-loop/Set C/for3.c
@@ -1,3 +1,22 @@
 //3. A train station A at 4.00 a.m. and travels at 80kmph. After every 30 minutes, it reaches a station where it halts for 10 minutes. 
 //it reaches its final destination B at 1.00 p.m.. Display a table showing its arrival and departure time at every intermediate station. 
 //also calculate the total distance between A and B.
+#include<stdio.h>
+int main()
+{
+    int time = 4.00 * 60;
+    int distance = 0;
+    int station = 0;
+    for(; time < 13 * 60; )
+    {
+        printf("Station %d:\n", station + 1);
+        printf("Arrival : %d:%d\n", time / 60, time % 60);
+        time = time + 30;
+        printf("Departure :%d:%d\n",time / 60, time % 60);
+        time = time + 10;
+        distance = distance + 40;
+        station++;
+    }
+    printf("Total Distance is : %d km\n",distance);
+    return 0;
+}

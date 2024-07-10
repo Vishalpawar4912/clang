ad16ad3318dedb55b7cb1a8c16595effc3a0a7b2
Vishalpawar4912 <pawarvvishal201@gmail.com>
2024-07-10 14:36:03 +0530

    Add do-while-loop problem and solution

--- 20240710/do-while-loop/Set C/do-while4.c
+++ 20240710/do-while-loop/Set C/do-while4.c
@@ -1,3 +1,8 @@
 //4. A task 4 1/2 hours to complete. Two workers A and B start working on it and take turns alternately. 
 //A works for 25 minutes at a time and is paid Rs. 50, B works for 75 minutes at a time and is paid Rs. 150. 
 //Display the total number of turns taken by A and B, calculate their total amounts and also the total cost of the task.
+#include<stdio.h>
+int main()
+{
+
+}

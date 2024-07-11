488050575083e3dbbfc6819413728bf2fd14eec2
Vishalpawar4912 <pawarvvishal201@gmail.com>
2024-07-11 18:00:04 +0530

    Add while-loop problem and solution

---
+++ 20240711/Book_problems/while_prob/while4.c
@@ -0,0 +1,28 @@
+// 4. To Display n lines of the following Triangle.,   
+//                                                       A   
+//                                                     A B A 
+//                                                   A B C B A 
+//                                                       .
+//                                                       .
+#include<stdio.h>
+int main()
+{
+    char ch;
+    int n, i = 1, j, spaces = 39;
+    printf("How Many Lines :");
+    scanf("%d", &n);
+    while (i <= n)
+    {
+        printf("%*s", spaces, " ");
+        spaces = spaces - 1;
+        for(j = 1, ch = 'A'; j <= i; j++, ch++)
+            printf("%c", ch);
+        ch = ch - 2;
+        for(j = 1; j <= i - 1; j++, ch--)
+            printf("%c", ch);
+        printf("\n");
+        i++;
+    }
+    return 0;
+}
+

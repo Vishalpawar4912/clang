fe17aca3d6fd0b6e1d4f60be9ca00db66805533f
Vishalpawar4912 <pawarvvishal201@gmail.com>
2024-07-11 12:29:12 +0530

    Add while_prob book proble and solution

---
+++ 20240711/Book_problems/while_prob/while2.c
@@ -0,0 +1,19 @@
+// 2.To display n lines of Floyd's Triangle : 1
+//                                            2 3
+//                                            4 5 6
+#include<stdio.h>
+int main()
+{
+    int n, i = 1, num, j;
+    printf("How many Lines :");
+    scanf("%d", &n);
+    num = 1;
+    while (i <= n)
+    {
+        for(j = 1; j <= i; j++)
+        printf("%d\t", num++);
+        printf("\n");
+        i++;
+    }
+    
+}

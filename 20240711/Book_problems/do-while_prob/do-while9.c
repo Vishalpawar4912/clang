efe667bdf1c89b7ea38c8f3e175a0717874c36ac
Vishalpawar4912 <pawarvvishal201@gmail.com>
2024-07-12 13:00:09 +0530

    Add do-while-loop problem and solution

---
+++ 20240711/Book_problems/do-while_prob/do-while9.c
@@ -0,0 +1,24 @@
+// 9. TO Display the Pattern. 
+// 1 2 3 4
+// 1 2 3
+// 1 2
+// 1
+#include<stdio.h>
+int main()
+{
+    int row, i, j;
+    printf("Enter the Number of Rows in thr Pattern :");
+    scanf("%d", &row);
+    i = row;
+    do
+    {
+        for(j = 1; j <= i; j++)
+        {
+            printf("%3d", j);
+        }
+        printf("\n");
+        i--;
+    } while (i > 0);
+    return 0;
+}
+

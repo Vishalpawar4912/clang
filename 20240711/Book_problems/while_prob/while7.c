c058b1cff2cc84c3bf7b893747f42c3523257288
Vishalpawar4912 <pawarvvishal201@gmail.com>
2024-07-12 12:45:21 +0530

    Add while-loop problem and solution

---
+++ 20240711/Book_problems/while_prob/while7.c
@@ -0,0 +1,20 @@
+//7. Program to accept a Number and check whether it is a Perfect Number (i.e. the Sum of it's factor = Number itself).
+#include<stdio.h>
+int main()
+{
+    int n, i = 1, sum = 0;
+    printf("Enter the Number :");
+    scanf("%d", &n);
+    while (i < n)
+    {
+        if(n % i == 0)
+        sum = sum + i;
+        i++;
+    }
+    if(sum == n)
+        printf("%d is Perfect Number", n);
+    else
+        printf("%d is not Perfect Number", n);
+    return 0;
+}
+

d29a4a2b8ea1ba376a3849369de74637ba8f3ef3
Vishalpawar4912 <pawarvvishal201@gmail.com>
2024-07-13 22:10:15 +0530

    Add while-loop problem and solution

---
+++ 20240711/Book_problems/while_prob/while12.c
@@ -0,0 +1,19 @@
+// 12.Write a C program that calculates and prints the sum of cubes of even numbers up to a specified limit (e.g., 20) using a while loop.
+#include<stdio.h>
+int main() 
+{
+    int i = 2, sum = 0, n, cube;
+    printf("Enter the limit for Even Numbers :");
+    scanf("%d", &n);
+    while (i <= n) 
+    {
+        if (i % 2 == 0) 
+        {
+            cube = i * i * i;
+            sum = sum + cube;
+        }
+        i = i + 2;
+    }
+    printf("Sum of cubes of even numbers up to %d: %d\n", n, sum);
+    return 0;
+}

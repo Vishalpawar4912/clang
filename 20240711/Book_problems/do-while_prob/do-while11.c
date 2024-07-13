eb2812cab1f2228a87b9601ce6a3afdf9f29c08c
Vishalpawar4912 <pawarvvishal201@gmail.com>
2024-07-13 22:09:44 +0530

    Add do-while-loop problem and solution

---
+++ 20240711/Book_problems/do-while_prob/do-while11.c
@@ -0,0 +1,16 @@
+//3. Write a C program that calculates the product of numbers from 1 to n using a while loop.
+#include<stdio.h>
+int main() 
+{
+    int n, i = 1, prod = 1;
+    printf("Enter the Number for product's Limit :");
+    scanf("%d", &n);
+    do
+    {
+        prod = prod * i;
+        printf("Current number: %d, Current product: %d\n", i, prod);
+        i++;
+    } while (i <= n);
+    printf("The product from 1 to %d is: %d\n", n, prod);
+    return 0;
+}

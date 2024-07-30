9be701b1f84192d344be73d5fbd0bf8ea1e89d80
Vishalpawar4912 <pawarvvishal201@gmail.com>
2024-07-30 12:05:58 +0530

    Add recursion function in c problem and solution

---
+++ 20240723/Recursion/Set C/recftoh.c
@@ -0,0 +1,6 @@
+//1. The "Towers of Hanoi" problem : The objective is to move a set of disks arranged in incresing sizes from top to bottom from the source pole 
+// to a destination pole such that they are in the same order as before using only one intermediate pole subject to the condition that :
+//    > Only one disk can be move at a time
+//    > A bigger disk cannot be place on smaller disk
+// Write a recursive function which displays all the steps to move n disk from A to C.
+#include<stdio.h>

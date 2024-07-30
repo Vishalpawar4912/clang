bbff6cab7ae8030be097b4a241a78eb757db94c8
Vishalpawar4912 <pawarvvishal201@gmail.com>
2024-07-30 12:32:38 +0530

    Add recursion fuction in c problem and solution

---
+++ 20240730/Recursion/recf1.c
@@ -0,0 +1,20 @@
+//1. Write a C program to find factorial of any number using recursion.
+#include<stdio.h>
+long factorial(int num) 
+{
+    if(num == 0 || num == 1)
+        return 1;
+    else
+        return num * factorial(num - 1);
+}
+int main() 
+{
+    int num;
+    printf("Enter the Number: ");
+    scanf("%d", &num);
+    if(num < 0)
+        printf("Factorial of Negative Numbers is not possible.");
+    else
+        printf("Factorial of %d : %ld", num, factorial(num));
+    return 0;
+}

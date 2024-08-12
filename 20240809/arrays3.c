6d62b04921733aa5ac1235347a8cd6f167e69a67
Vishalpawar4912 <pawarvvishal201@gmail.com>
2024-08-12 12:02:56 +0530

    Add array problem and solution

---
+++ 20240809/arrays3.c
@@ -0,0 +1,21 @@
+//3. Write a C program to find sum of all array elements. – using recursion.
+#include <stdio.h>
+#define MAX 100
+int main()
+{
+    int arr[MAX];
+    int i, n, sum = 0;
+    printf("Enter size of the array: ");
+    scanf("%d", &n);
+    printf("Enter %d elements in the array:\n", n);
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    for(i = 0; i < n; i++)
+    {
+        sum = sum + arr[i];
+    }
+    printf("Sum of all elements of array = %d", sum);
+    return 0;
+}

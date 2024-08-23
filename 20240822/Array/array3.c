18ed9881f017f4b602759fa67daa732b281d737d
Vishalpawar4912 <pawarvvishal201@gmail.com>
2024-08-23 18:26:18 +0530

    Add array problem and solution

---
+++ 20240822/Array/array3.c
@@ -0,0 +1,44 @@
+//3. To accept a m x n matrix and generate a m + 1 x n + 1 matrix such that the mth row contains sum of elements of corresponding columns and 
+//   the nth column contains sum of elements of corresponding rows.
+//    Example. 
+//              Input                     Output
+//            1   2   3              1    2    3    6
+//            4   5   6              4    5    6   15
+//            7   8   9              7    8    9   24
+//                                  12   15   18   45
+#include<stdio.h>
+int main()
+{
+    int a[10][10], i, j, m, n, sum;
+    printf("Enter the number of rows and columns:\n");
+    scanf("%d %d", &m, &n);
+    printf("Enter the elements of matrix:\n");
+    for(i = 0; i < m; i++) 
+    {
+        sum = 0;
+        for(j = 0; j < n; j++) 
+        {
+            scanf("%d", &a[i][j]);
+            sum = sum + a[i][j];
+        }
+        a[i][n] = sum; 
+    }
+    for(j = 0; j < n; j++) 
+    { 
+        sum = 0;
+        for(i = 0; i < m; i++)
+            sum = sum + a[i][j];
+        a[m][j] = sum;
+    }
+    a[m][n] = 0; 
+    for(i = 0; i < m; i++)
+        a[m][n] += a[i][n];
+    printf("The Resultant Matrix is: \n");
+    for(i = 0; i <= m; i++) 
+    {
+        for(j = 0; j <= n; j++)
+            printf("%5d", a[i][j]);
+        printf("\n");
+    }
+    return 0;
+}

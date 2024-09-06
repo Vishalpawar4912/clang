792522f854d53727a32a5e7497c0d6a5887676b9
Vishalpawar4912 <pawarvvishal201@gmail.com>
2024-09-06 16:15:50 +0530

    Add strings problem and soution

--- 20240905/Strings/strings8.c
+++ 20240905/Strings/strings8.c
@@ -1,7 +1,27 @@
-// 8. Program which replaces all occurrences of a character in a string by another character. 
-//    The string and characters are accepted as command line arguments.
-#include<stdio.h>
-int main()
-{
-    
-}
+// // 8. Program which replaces all occurrences of a character in a string by another character. 
+// //    The string and characters are accepted as command line arguments.
+
+
+
+// #include <stdio.h>
+// #include <string.h>
+// int main(int argc, char *argv[]) 
+// {
+//     if (argc != 4) 
+//     {
+//         printf("Usage: %s <string> <char_to_replace> <char_to_replace_with>\n", argv[0]);
+//         return 1;
+//     }
+//     char *str = argv[1];
+//     char char_to_replace = argv[2][0];
+//     char char_to_replace_with = argv[3][0];
+//     for (int i = 0; i < strlen(str); i++) 
+//     {
+//         if (str[i] == char_to_replace) 
+//         {
+//             str[i] = char_to_replace_with;
+//         }
+//     }
+//     printf("Modified string: %s\n", str);
+//     return 0;
+// }
--- 20240905/Strings/strings9.c
+++ 20240905/Strings/strings9.c
@@ -1,8 +1,35 @@
-// 9. Program which encrypts a string by an integer key. The string and key are accepted as command line arguments. 
-//    The key will be sorted as a string. Hence we have to convert it to an integer.
-#include<stdio.h>
-#include<stdlib.h>
-int main()
-{
-    
-}
+// // 9. Program which encrypts a string by an integer key. The string and key are accepted as command line arguments. 
+// //    The key will be sorted as a string. Hence we have to convert it to an integer.
+
+
+
+// #include <stdio.h>
+// #include <string.h>
+// #include <stdlib.h>
+// void encrypt(char *str, int key) {
+//     for (int i = 0; i < strlen(str); i++) 
+//     {
+//         if (str[i] >= 'a' && str[i] <= 'z') 
+//         {
+//             str[i] = ((str[i] - 'a' + key) % 26) + 'a';
+//         } 
+//         else if (str[i] >= 'A' && str[i] <= 'Z') 
+//         {
+//             str[i] = ((str[i] - 'A' + key) % 26) + 'A';
+//         }
+//     }
+// }
+// int main(int argc, char *argv[]) 
+// {
+//     if (argc != 3) 
+//     {
+//         printf("Usage: %s <string> <key>\n", argv[0]);
+//         return 1;
+//     }
+//     char *str = argv[1];
+//     char *key_str = argv[2];
+//     int key = atoi(key_str);
+//     encrypt(str, key);
+//     printf("Encrypted string: %s\n", str);
+//     return 0;
+// }
